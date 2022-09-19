ef17b2f85b960e7c7c20093d4280e241c2eb31c0
Carlos Sousa <carlos.sousa@youxgroup.com.br>
2022-09-19 10:12:15 -0300

    implementado nova forma de clonar repositorio usando command do jgit

--- src/main/java/br/com/meta/api/repository/GitRepository.java
+++ src/main/java/br/com/meta/api/repository/GitRepository.java
@@ -2,11 +2,16 @@ package br.com.meta.api.repository;
 
 import java.io.File;
 
+import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.JGitInternalException;
 import org.jboss.logging.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
+import br.com.meta.api.exception.GitCloneException;
+import br.com.meta.api.exception.RepositorioNaoExisteException;
 import br.com.meta.api.model.Autor;
 
 @Repository
@@ -17,21 +22,21 @@ public class GitRepository {
 	
 	public void clonar(String usuario, String repositorio, File diretorio) {
 		
-//		try {
+		try {
 			RestTemplate rt = new RestTemplate();
 			ResponseEntity<Autor> response = rt.getForEntity(String.format(BASE_GITHUB_API, usuario, repositorio), Autor.class);
 			String url = response.getBody().getHtml_url().concat(".git");
 			
-//			CloneCommand command = new CloneCommand();
-//			command.setURI(url);
-//			command.setDirectory(diretorio);
-//			command.call();
-//		} catch (GitAPIException e) {
-//			throw new GitCloneException();
-//		} catch (JGitInternalException e) {
-//			// Não deve fazer nenhum tratamento para essa exception.
-//		} catch (Exception e) {
-//			throw new RepositorioNaoExisteException();
-//		}
+			CloneCommand command = new CloneCommand();
+			command.setURI(url);
+			command.setDirectory(diretorio);
+			command.call();
+		} catch (GitAPIException e) {
+			throw new GitCloneException();
+		} catch (JGitInternalException e) {
+			// Não deve fazer nenhum tratamento para essa exception.
+		} catch (Exception e) {
+			throw new RepositorioNaoExisteException();
+		}
 	}
 }
